570f91a05ba09b6e62a2abd354f2de4cfa2bf974
Lee Goddard <lee@leegoddard.net>
2015-11-06 15:41:05 +0100

    OMDb API

--- tests/Omdb.js
+++ tests/Omdb.js
@@ -6,25 +6,26 @@ var chai     = require('chai'),
     expect   = chai.expect,
     should   = require('chai').should(),
     log4js   = require('Log4js'),
-    Dbpedia  = require('../lib/Dbpedia.js')
+    Omdb     = require('../lib/Omdb.js'),
+    sqlite3  = require('sqlite3').verbose()
 ;
 
 log4js.replaceConsole();
 
-describe('Dbpedia class', function (){
+describe('Omdb class', function (){
     it( 'is as expected', function () {
-        var agent = new Dbpedia();
+        var agent = new Omdb();
         should.equal( typeof agent, "object", "Construted" );
-        agent.should.be.instanceof(Dbpedia, "Construted class" );
+        agent.should.be.instanceof(Omdb, "Construted class" );
         should.equal( typeof agent.get, "function", "has method 'get'" );
     });
 
-    it( 'gets generes for TV', function (done) {
-        new Dbpedia().get('NCIS', function (genres){
+    it( 'gets generes for Movies', function (done) {
+        new Omdb().get('Jaws', function (genres){
             should.equal(typeof genres, 'object', 'genres list');
             genres.should.be.instanceof(Array, 'genres list');
-            genres.should.have.length(2);
-            genres.should.include('Drama', 'Action_(fiction)');
+            genres.should.have.length(3);
+            genres.should.include('Thriller','Drama','Adventure');
             done();
         });
     });
