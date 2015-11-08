e68dd63e326cc6ba7030b57393483e505015dea0
Lee Goddard <lee@leegoddard.net>
2015-11-08 21:04:28 +0100

    Testing

--- tests/Torrent.js
+++ tests/Torrent.js
@@ -43,7 +43,8 @@ describe('Torrent', function (){
         torrent.katFields.torrent_category = 'TV';
         torrent.katFields.torrent_info_url = 'https://kat.cr/ncis-s13e07-hdtv-x264-lol-ettv-t11522935.html';
         torrent.save( function (){
-            Torrent.showAll( torrent.options.db, done );
+            // Torrent.showAll( torrent.options.db, done );
+            done();
         });
     });
 });
