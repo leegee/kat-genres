b8282c341e56668e95b9a00ec6e66baac1d5d54b
Lee Goddard <lee@leegoddard.net>
2015-11-11 11:18:30 +0100

    add init es

---
+++ bin/es.js
@@ -0,0 +1,30 @@
+var elasticsearch = require('elasticsearch');
+var config = require('../package.json');
+
+var client = new elasticsearch.Client({
+  host: 'localhost:'+config.elasticsearch.port,
+  log: 'trace'
+});
+
+client.indices.delete({
+    index: "torrents"
+}).then( function (err, resp, respcode) {
+    console.log(err, resp, respcode);
+    client.indices.create({
+        index: "torrents",
+        body: {
+            "mappings": {
+                "torrent": {
+                    "properties": {
+                        "id": {"type": "string", "index": "not_analyzed"},
+                        "title": {"type": "string", "index": "not_analyzed"},
+                        "genres": {"type": "string", "index": "not_analyzed"},
+                        "torrent_info_url": {"type": "string", "index": "not_analyzed"}
+                    }
+                }
+            }
+        }
+    })
+}).then( function (err, resp, respcode) {
+    console.log(err, resp, respcode);
+})
