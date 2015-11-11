276d06d15a6cfad813a2f93fcb3301eada38ae91
Lee Goddard <lee@leegoddard.net>
2015-11-11 12:44:13 +0100

    setup

---
+++ bin/es-populate.js
@@ -0,0 +1,27 @@
+var elasticsearch = require('elasticsearch');
+var config        = require('../package.json');
+
+var client = new elasticsearch.Client({
+    host: 'localhost:'+config.elasticsearch.port,
+    log: 'trace',
+    apiVersion: "2.1"
+});
+
+client.indices.delete({
+    index: "torrents",
+    ignore: [404]
+}).then( function (err, resp, respcode) {
+    console.log(err, resp, respcode);
+    client.index({
+        index: "torrents",
+        type: "torrrent",
+        body: {
+            "id":     "1",
+            "title":  "Test",
+            "genres": ['test','foo'],
+            "torrent_info_url": 'http://foo.com'
+        }
+    })
+}).then( function (err, resp, respcode) {
+    console.log(err, resp, respcode);
+})
