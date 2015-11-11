1de81b34f9315db0f4261aa24d085ebba85bb058
Lee Goddard <lee@leegoddard.net>
2015-11-11 17:22:18 +0100

    Index

--- bin/service.js
+++ bin/service.js
@@ -24,6 +24,7 @@ app.get('/all', function (conn) {
                     [row.torrent_name, row.title,row.genres].join("\t")+"\n"
                 );
             });
+            // db.close();
         }
     });
 });
