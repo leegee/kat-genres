ed791aa1917845c0fe2eec7a042bf186c553ddd3
Lee Goddard <lee@leegoddard.net>
2015-11-18 11:58:27 +0100

    Sep config

--- public/index.js
+++ public/index.js
@@ -1,49 +1,17 @@
-require.config({
-    baseUrl: '/public/',
-    shim: {
-        'jQuery': {
-            exports: 'jQuery'
-        },
-        'underscore': {
-            exports: '_'
-        },
-        'elasticsearch': {
-            deps: [
-                'jQuery'
-            ]
-        },
-        'backbone': {
-            deps: [
-                'underscore',
-                'jQuery'
-            ],
-            exports: 'backbone'
-        }
-    },
-    map: {
-        '*': {
-            jQuery: 'jQuery',
-            backbone: 'backbone',
-            underscore: 'underscore'
-        }
-    },
-    paths: {
-        elasticsearch: 'bower/elasticsearch/elasticsearch.jquery.min',
-        jQuery:        'bower/jquery/dist/jquery.min',
-        underscore:    'bower/underscore/underscore-min',
-        backbone:      'bower/backbone/backbone-min'
-        // text: '../lib/require/text',
-    }
-});
-
 requirejs([
-    'jQuery', 'lib/Router.js'
+    'Config.js'
 ], function (
-    jQuery, Router
+    Config
 ) {
-    'use strict';
-    jQuery(document).ready( function () {
-        new Router().start();
+    requirejs([
+       'jQuery', 'lib/Router.js'
+    ], function (
+        jQuery, Router
+    ) {
+        'use strict';
+        jQuery(document).ready( function () {
+            new Router().start();
+        });
     });
 });
 
