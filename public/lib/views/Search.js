6be4baf2621c88c4130b65e1869dd7a3ebf60bd6
Lee Goddard <lee@leegoddard.net>
2015-11-20 10:08:34 +0100

    order

--- public/lib/views/Search.js
+++ public/lib/views/Search.js
@@ -17,26 +17,9 @@
 
         initialize: function () {
             this.collection = new Collection();
-            this.$root    = jQuery('#app-container');
-            this.template = _.template( jQuery('#template-list').text() );
-        },
-
-        render: function (esRes) {
-            esRes = esRes || [];
-            if (! jQuery.contains(document, this.$el[0])) {
-                this.$root.append( this.$el );
-            }
-            this.$el.empty();
-            this.$el.html(
-                this.template({
-                    collection : esRes.hits,
-                    terms      : this.terms,
-                    page       : this.page,
-                    totalPages : esRes.totalHits / this.pageSize,
-                    took       : esRes.took
-                })
-            );
-            document.dispatchEvent(eventRendered);
+            this.$root      = jQuery('#app-container');
+            this.$resultsEl = jQuery('#results');
+            this.template   = _.template( jQuery('#template-list').text() );
         },
 
         events: {
@@ -54,12 +37,36 @@
 
         search: function (terms, page) {
             var self = this;
-            this.terms = terms;
-            this.page  = page;
+            this.terms = terms || null;
+            this.page  = page || 0;
             this.collection.fetch(this.terms, this.pageSize, this.page).then( function (res){
                 self.render(res);
             } );
+        },
+
+        render: function (esRes) {
+            esRes = esRes || [];
+            if (! jQuery.contains(document, this.$el[0])) {
+                this.$root.append( this.$el );
+            }
+
+            // If rendering the first page, repopulate the template
+            // If adding results, add children to the result element
+            if (this.page===0){
+                this.$el.empty();
+                this.$el.html(
+                    this.template({
+                        collection : esRes.hits,
+                        terms      : this.terms,
+                        page       : this.page,
+                        totalPages : esRes.totalHits / this.pageSize,
+                        took       : esRes.took
+                    })
+                );
+            }
+            document.dispatchEvent(eventRendered);
         }
+
     });
 });
 
